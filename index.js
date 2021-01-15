265182dcb1066a78904f24e039a105c9262c882d
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-01-15 11:36:20 -0700

    commit tree c77c50c99534ee39a0960e16125d6c7078c0b5dd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let new_array = []
+    for(let i=0; i <array.length; i++){
+        new_array.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return new_array
+}
+
+console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+function countDown(num) {
+    while (num >= 0){
+        console.log(num--)
+
+    }
+}
